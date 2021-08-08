f176a6adb272f91b27b19f62fcc7028345caa15e
ohbarye <over.rye@gmail.com>
2021-08-08 15:53:05 +0900

    Replace action example
    https://docs.github.com/ja/actions/creating-actions/creating-a-javascript-action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet')
+    console.log(`Hello ${nameToGreet}!`)
+    const time = new Date().toTimeString()
+    core.setOutput('time', time)
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`)
   } catch (error) {
     core.setFailed(error.message)
   }
