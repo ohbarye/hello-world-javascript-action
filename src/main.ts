afc8cd9af77b8c16fa5c6de3180fdc6e5b341d32
ohbarye <over.rye@gmail.com>
2021-08-08 16:07:40 +0900

    Pass lint

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ async function run(): Promise<void> {
   try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet')
-    console.log(`Hello ${nameToGreet}!`)
+    console.log(`Hello ${nameToGreet}!`) // eslint-disable-line no-console
     const time = new Date().toTimeString()
     core.setOutput('time', time)
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
+    console.log(`The event payload: ${payload}`) // eslint-disable-line no-console
   } catch (error) {
     core.setFailed(error.message)
   }
